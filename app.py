f50dca2b39ff789c26dc3870617b4a774e0c33ca
Andrew Hyder <hyd415@gmail.com>
2013-03-23 15:22:17 -0500

    Loads example data.

--- app.py
+++ app.py
@@ -7,14 +7,23 @@ import os
 
 app = Flask(__name__)
 
+
+# Calls an example API to get some data from cartodb
+
+longitude = str(-94.58083)
+latitude = str(39.08971)
+
 @app.route('/neighborhood')
 def neighborhood():
 	url = 'http://cfa.cartodb.com/api/v2/sql?q='
-	url = url + 'SELECT%20nbhname%20FROM%20kc_census_hoods%20WHERE%20ST_CONTAINS(the_geom,%20ST_GeomFromText(\'POINT(-94.58083%2039.08971)\',%204326))'
+	url = url + 'SELECT%20nbhname%20FROM%20kc_census_hoods%20WHERE%20'
+	url = url + 'ST_CONTAINS(the_geom,%20ST_GeomFromText(\'POINT('+longitude+'%20'+latitude+')\',%204326))'
 	r = requests.get(url)
-	rjson = r.json()
+	rjson = r.json
 	nbhname = rjson['rows'][0]['nbhname']
 	return render_template('neighborhood.html', nbhname = nbhname)
+
+# ToDo: Google Earth template that zooms to this neighborhood
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
