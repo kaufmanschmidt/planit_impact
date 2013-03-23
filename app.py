db35317eb91e868d523f48654fd7beb5751ad8e7
Andrew Hyder <hyd415@gmail.com>
2013-03-23 15:08:18 -0500

    Working on pulling down example data and displaying it.

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+import requests
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
-	return render_template('index.html')
+@app.route('/neighborhood')
+def neighborhood():
+	url = 'http://cfa.cartodb.com/api/v2/sql?q='
+	url = url + 'SELECT%20nbhname%20FROM%20kc_census_hoods%20WHERE%20ST_CONTAINS(the_geom,%20ST_GeomFromText(\'POINT(-94.58083%2039.08971)\',%204326))'
+	r = requests.get(url)
+	rjson = r.json()
+	nbhname = rjson['rows'][0]['nbhname']
+	return render_template('neighborhood.html', nbhname = nbhname)
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
