da4893a18c0224c33a06a4238623386f270887c0
Andrew Hyder <hyd415@gmail.com>
2013-05-14 19:49:41 -0700

    Remade into a flask app!

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/features.html")
+def features():
+    return render_template('features.html')
+
+@app.route("/howitworks.html")
+def howitworks():
+    return render_template('howitworks.html')
+
+@app.route("/newproject.html")
+def newproject():
+    return render_template('newproject.html')
+
+if __name__ == "__main__":
+    app.run()
