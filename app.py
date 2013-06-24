2115d2975033b16dc68bb1814616186ac99b9b50
blackops <jbell@blackopsdev.com>
2013-06-24 13:40:23 +0400

    added deletion of projects small design fixes

--- app.py
+++ app.py
@@ -173,6 +173,16 @@ def float_or_zero(int_str):
     except:
         return 0
 
+@app.route("/projects/<model_id>/delete", methods=['POST'])
+def project_delete(model_id):
+    model = Project.query.filter_by(id=model_id).first()
+
+    db.session.delete(model)
+    db.session.commit()
+
+    all_projects = Project.query.order_by(Project.id.desc()).all()
+    return render_template('demo_projects.html', all_projects=all_projects)
+
 @app.route("/projects/<model_id>/", methods=['GET', 'POST'])
 def project(model_id):
     model = Project.query.filter_by(id=model_id).first()
@@ -207,7 +217,16 @@ def project(model_id):
     try:
         settings_json = json.loads(model.settings_json)
     except:
-        settings_json = { 'c1': 0.3, 'c2': 0.9, 'c3': 0.1, 'c4': 0.5, 'c5': 0.15, 'c6': 0.75 }
+        if model.kmz_url:
+            settings_json = { 'c1': 0.3, 'c2': 0.9, 'c3': 0.1, 'c4': 0.5, 'c5': 0.15, 'c6': 0.75,
+                              'c1_area_p': 0,
+                              'c2_area_p': 20,
+                              'c3_area_p': 10,
+                              'c4_area_p': 25,
+                              'c5_area_p': 30,
+                              'c6_area_p': 15, }
+        else:
+            settings_json = {}
 
     return render_template('project.html', model=model, settings=settings_json)
 
@@ -243,11 +262,11 @@ def project_overlay(model_id):
         c = placemark.get('class')
         sub_value = params.get(c)
 
-        if sub_value:
+        if sub_value is not None:
             color = placemark.find('./{http://www.opengis.net/kml/2.2}Style/{http://www.opengis.net/kml/2.2}PolyStyle/{http://www.opengis.net/kml/2.2}color')
             if color is not None:
                 if sub_value < 0.5:
-                    sub_value = 256 * (1 - sub_value * 2)
+                    sub_value = 256 * (sub_value * 2)
                     sub_value = min(sub_value, 255)
                     sub_value = max(sub_value, 0)
 
